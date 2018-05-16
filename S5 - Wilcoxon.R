5bebc342c81fb8941a5db7e3a4a6578af8d0e71d
Carlos Neftaly <neftaly.lozano@gmail.com>
2018-05-16 14:19:44 -0500

    Wilcoxon agregado dentro del mismo proyecto.

--- S5 - Wilcoxon.R
+++ S5 - Wilcoxon.R
@@ -1,5 +1,5 @@
 #' ---
-#' title: "Sesión 5 Kruskal - Pruebas no paramétricas"
+#' title: "Sesión 5 Wilcoxon - Pruebas no paramétricas"
 #' author: "Nombre y apellido"
 #' date: "Mes día,  año"
 #' ---
@@ -11,8 +11,6 @@
 # Cargar paquetes para análisis
 library(lattice)
 library(car)
-require(multcomp)
-require(agricolae)
 library(knitr)
 
 
@@ -22,7 +20,7 @@ library(knitr)
 
 
 # Carga de archivo .csv: 
-datos<-read.csv('Datos/Kruskal.csv',header=T,sep=';')
+datos<-read.csv('Datos/Wilcoxon.csv',header=T,sep=';')
 attach(datos)
 
 # Para verificar la correcta importación de los datos se llama por el nuevo nombre:datos
@@ -31,63 +29,64 @@ attach(datos)
 ###############################ANÁLISIS EXPLORATORIOS#####################################################
 # Análisis exploratorio: Boxplot superpuesto con stripchart
 # Genera un gráfico de bigotes buscando tendecias (tendecia central, dispersión y simetría)
-# Porcentaje de Mortalidad
-boxplot(Pmortalidad~Tratamiento)
-stripchart(Pmortalidad~Tratamiento,vert=T,pch=21,col='red',add=T)
+boxplot(Especies~Fertilidad,las=2)
+stripchart(Especies~Fertilidad,vert=T,pch=21,col='red',add=T)
 
 # Genera un histograma de la variable respuesta
-hist(Pmortalidad,prob=T)
-lines(density(Pmortalidad))
+hist(Especies,prob=T)
+lines(density(Especies))
+
+
 
 # Ajuste de modelo lineal general
 # Este modelo relaciona la variable respuesta en función de un solo factor
-modelopm<-lm(Pmortalidad~Tratamiento)
+# Pueden existir dos formas de hacerlo: con el comando lm o aov
+modelo1<-lm(Especies~Fertilidad)
 
 # Para la visualización de los parámetros etimados con le modelo:
-summary(modelopm)
+summary(modelo1)
+
 
 
 
 ################################EVALUACIÓN DE SUPUESTOS#############################################
 ##EVALUACIÓN DE NORMALIDAD##
-
-shapiro.test(modelopm$residuals)
-
-
+#Se continuará trabajando con modelo1 (lm)
+shapiro.test(modelo1$residuals)
 # Análisis gráfico del modelo
 # El comando "par" genera panales 2*2
 par(mfrow=c(2,2))
 # Gráfico de cuantiles
-plot(modelopm)
+plot(modelo1)
 
 # Con el comando "par" se vuelve a generar panel de 1*1
 par(mfrow=c(1,1))
 
 
-#EVALUACIÓN DE HOMOGENEIDAD DE VARIANZAS##
+##EVALUACIÓN DE HOMOGENEIDAD DE VARIANZAS##
 ##Extraccion de los residuales
-residuales<-resid(modelopm)
+residuales<-resid(modelo1)
 #Gráfico de cajas y bigotes
-boxplot(residuales~Tratamiento, data=datos, ylim=c(-6,6), ylab="Residuales", xlab="Tratamiento", main="Homogeneidad de residuales") 
+boxplot(residuales~Fertilidad, data=datos, ylim=c(-6,6), ylab="Residuales", xlab="Tratamiento", main="Homogeneidad de residuales") 
 
+#Gráfico de residuales dispersos por tratammiento
+#Nota: Aunque se requiere el paquete lattice, este ya se encuentra activo por un paso anterior
+
+stripplot(residuales~Fertilidad, data=datos, ylim=c(-6,6), ylab="Residuales", xlab="Tratamiento", main="Homogeneidad de residuales")
 
 # Homocedasticidad Test de Levene 
-leveneTest(Pmortalidad~Tratamiento)
+
+leveneTest(modelo1)
 
 # Homocedasticidad Test de Bartlett 
-bartlett.test(Pmortalidad~Tratamiento) 
+bartlett.test(Especies~Fertilidad)     
+
 
 #NOTA: Recuerde que los residuales deben cumplir con los supuestos necesarios
 #para que el ANOVA sea válido. En caso de que el análisis de residuales muestre
 #problemas a este respecto, la variable respuesta debe ser transformada hasta
-#que los residuales del modelo ajusten a los supuestos
+#que los residuales del modelo ajusten a los supuestos. 
 
 
-#Test de Kruskal
-#Primera manera de hacerlo 
-kruskal.test(Pmortalidad~Tratamiento)
-lslsls
-#Segunda forma de ejecutarlo (se requiere el paquete Agricolae)
-#Muestra comparaciones multiples no paramétricas
-kruskal(Pmortalidad, Tratamiento, console=T)
-
+#Wilcoxon
+wilcox.test(Especies~Fertilidad)
